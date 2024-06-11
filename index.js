b8a2ee49fd61e533bbc49098678bbb5add2d0e55
Atharva Tyagi <atharvatyagi80@gmail.com>
2024-06-11 18:33:30 +0530

    created card project

---
+++ index.js
@@ -0,0 +1,9 @@
+const saveChanges = ()=>{
+    const taskData = {
+        id: `${Date.now()}`,
+        imageUrl:document.getElementById("imageUrl"),
+        taskTitle: document.getElementById("taskTitle"),
+        taskType: document.getElementById("taskType"),
+        taskDescription: document.getElementById("taskDescription"),
+    };
+}
